27d453e81dce7161b02b7d090581a846797026a7
Daisy Galvão <daisy.smgalvao@gmail.com>
2022-10-31 16:10:46 -0300

    Comando_USE

---
+++ Comando USE.sql
@@ -0,0 +1,12 @@
+--Comando USE
+
+--O comando serve para quando for executar comandos abaixo, vai entender que é no banco de dados especificado
+
+USE db_Biblioteca
+
+
+sp_helpdb db_Biblioteca
+
+--sp_helpdb
+
+--Comando interno do SQL SERVER
