3503ca9f4bd3a9405fe8af81dbc56561bf18453d
Daisy Galvão <daisy.smgalvao@gmail.com>
2022-11-21 22:22:27 -0300

    ORDER_BY

---
+++ EXPRESSÃO ORDER BY.sql
@@ -0,0 +1,17 @@
+--EXPRESSAO ORDER BY
+
+--Serve para ordenar o conjunto de registros
+
+--Sendo ele de forma:
+
+--ASC - Ordem Ascendente
+--DESC - Ordem descendente (inversa)
+
+USE db_Biblioteca
+
+--SELECT * FROM tbl_Livro ORDER BY Nome_Livro
+
+--SELECT * FROM tbl_Livro ORDER BY Nome_Livro DESC
+
+SELECT Nome_autor FROM tbl_autores
+ORDER BY Nome_Autor
