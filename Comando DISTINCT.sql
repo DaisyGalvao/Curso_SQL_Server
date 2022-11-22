f12e429041153f5bb4adb0ee234a0595c05e6867
Daisy Galvão <daisy.smgalvao@gmail.com>
2022-11-21 22:23:08 -0300

    DISTINCT

---
+++ Comando DISTINCT.sql
@@ -0,0 +1,13 @@
+--Comando DISTINCT
+
+--Utilizado para exibir valores diferentes quando faz uma consulta
+--Exibe apenas valores individuais
+
+USE db_Biblioteca
+
+SELECT ID_Autor
+FROM tbl_Livro
+
+
+SELECT DISTINCT ID_Autor
+FROM tbl_Livro
