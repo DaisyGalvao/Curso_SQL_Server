da2e5b4db38ef684cc6a7b7e0d2470505f6ca294
Daisy Galvão <daisy.smgalvao@gmail.com>
2022-11-21 22:17:47 -0300

    CREATE_TABLE

---
+++ CREATE TABLE.sql
@@ -0,0 +1,31 @@
+--CREATE TABLE
+
+USE db_Biblioteca
+
+CREATE TABLE tbl_Livro
+(ID_Livro SMALLINT PRIMARY KEY IDENTITY(100,1),
+Nome_Livro VARCHAR (50) NOT NULL,
+ISBN VARCHAR(30) NOT NULL UNIQUE,
+ID_Autor SMALLINT NOT NULL,
+Data_Pub DATETIME NOT NULL,
+Preco_Livro MONEY NOT NULL)
+
+
+
+CREATE TABLE tbl_autores (
+ID_Autor SMALLINT PRIMARY KEY,
+Nome_Autor VARCHAR(50),
+Sobrenome_Autor VARCHAR(60)
+)
+
+CREATE TABLE tbl_editoras
+(ID_Editora SMALLINT PRIMARY KEY IDENTITY,
+Nome_Editora VARCHAR(50) NOT NULL
+)
+
+
+--sp_help tbl_Livro
+
+
+
+
