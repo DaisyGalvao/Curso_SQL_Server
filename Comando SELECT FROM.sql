ddf4ee198dc3db466da9ab0d2bb099aca51068bc
Daisy Galvão <daisy.smgalvao@gmail.com>
2022-11-21 22:21:24 -0300

    SELECT_FROM

---
+++ Comando SELECT FROM.sql
@@ -0,0 +1,11 @@
+--Comando SELECT FROM
+
+--Serve para consulta a uma tabela ou mais
+
+USE db_Biblioteca
+
+--SELECT Nome_Autor FROM tbl_autores
+
+--SELECT * FROM tbl_Livro
+
+SELECT Nome_Autor, Sobrenome_autor FROM tbl_autores
